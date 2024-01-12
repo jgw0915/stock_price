452aee2372c141ff9071fee6c9af9a7bd2623631
jgw0915 <121889006+jgw0915@users.noreply.github.com>
2024-01-13 00:59:18 +0800

    Refector: Move Application to Class- StockPriceApplication

--- src/main/java/com/example/stock_price/StockPriceApplication.java
+++ src/main/java/com/example/stock_price/StockPriceApplication.java
@@ -1,8 +1,11 @@
 package com.example.stock_price;
+import com.example.stock_price.GUI.Template_frame;
+
 public class StockPriceApplication {
 
     public static void main(String[] args) {
-
+        System.setProperty("file.encoding", "UTF-8");
+        Template_frame frame = new Template_frame();
     }
 
 }
